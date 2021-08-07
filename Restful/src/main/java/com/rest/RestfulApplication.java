df2977587b18e2d06c4220046e5c2dc31448bf43
Administrator <Administrator@WIN-RHBT4NTFUDH>
2021-08-08 00:35:23 +0530

    commit myProjects

--- Restful/src/main/java/com/rest/RestfulApplication.java
+++ Restful/src/main/java/com/rest/RestfulApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RestfulApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(RestfulApplication.class, args);
 	}
